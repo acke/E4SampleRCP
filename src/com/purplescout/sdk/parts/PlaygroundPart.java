e986b040addef58b232a7fb3d8654683f1ab5c08
Knut Funkel <knut.funkel@purplescout.se>
2013-10-09 10:28:49 +0200

    Updated PlaygroundPart.java with controls in Window Builder
    initial add
    
    adding jdt prefs

--- src/com/purplescout/sdk/parts/PlaygroundPart.java
+++ src/com/purplescout/sdk/parts/PlaygroundPart.java
@@ -7,18 +7,61 @@ import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class PlaygroundPart {
 
 	Label label;
+	private Text text;
 
 	@PostConstruct
 	public void createControls(Composite parent) {
+		parent.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent e) {
+			}
+		});
+		parent.setLayout(new GridLayout(3, false));
 		label = new Label(parent, SWT.None);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		
+		Button btnNewButton = new Button(parent, SWT.NONE);
+		btnNewButton.setText("New Button");
+		new Label(parent, SWT.NONE);
+		new Label(parent, SWT.NONE);
+		
+		Button btnNewButton_1 = new Button(parent, SWT.NONE);
+		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+		});
+		btnNewButton_1.setText("New Button");
+		new Label(parent, SWT.NONE);
+		
+		Label lblNewLabel = new Label(parent, SWT.NONE);
+		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblNewLabel.setText("New Label");
+		
+		text = new Text(parent, SWT.BORDER);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 	}
 
 	@PreDestroy
-	private void dispose() {
+	public void dispose() {
 		// TODO Auto-generated method stub
 
 	}
